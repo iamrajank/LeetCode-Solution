7f3884bd3dd1d9bad2e707dbdafecb980a413080
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2024-11-23 11:33:19 +0530

    Time: 15 ms (27.75%), Space: 16.8 MB (46.52%) - LeetHub

---
+++ 1044-find-common-characters/1044-find-common-characters.py
@@ -0,0 +1,23 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        d=dict()
+        res=[]
+        for ele in words[0]:
+            if ele not in d:
+                d[ele]=[1]
+            else:
+                d[ele][0]+=1
+        for i in range(1,len(words)):
+            for ele in words[i]:
+                if ele in d.keys():
+                    if len(d[ele])==i:
+                        d[ele].append(1)
+                    else:
+                        d[ele][-1]+=1
+        for key in d.keys():
+            if len(d[key])==len(words):
+                temp=key*min(d[key])
+                temp.split()
+                res+=temp
+        return res
+        

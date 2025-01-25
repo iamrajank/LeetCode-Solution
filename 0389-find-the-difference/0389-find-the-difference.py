7e086bead98c3a9f84b1eda6e9239ae2da2acfe3
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2025-01-25 22:20:40 +0530

    Time: 3 ms (53.69%), Space: 17.6 MB (60.25%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,8 +2,22 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
 
         # Brute force
+        # for i in t:
+        #     if t.count(i) > s.count(i):
+        #         return i
+
+        temp = {}
         for i in t:
-            if t.count(i) > s.count(i):
+            if i not in temp:
+                temp[i] = 1
+            else:
+                temp[i] += 1
+        for i in s:
+            temp[i] = temp[i] - 1
+        for i in temp:
+            if temp[i] == 1:
                 return i
+
+
       
         

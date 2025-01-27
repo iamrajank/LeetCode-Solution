ba80490227ad591ed6d1e8d43566f81962f0655b
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2025-01-28 00:51:58 +0530

    Time: 11 ms (25.14%), Space: 25.7 MB (16.38%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,45 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # temp = []
-        # n = len(nums)
-        # k = k % n
-        # for i in range(len(nums)-1,-1,-1):
-        #     temp.append(nums[i])
-        # r_temp = []
-        # for i in range(k-1,-1,-1):
-        #     r_temp.append(temp[i])
-        # for i in range(len(nums)-1,k-1,-1):
-        #     r_temp.append(temp[i])
-        
-        # nums.clear()
-        # for i in r_temp:
-        #     nums.append(i)
-        # return nums
-
-        # Two pointer 
-
         n = len(nums)
-        k = k % n
         i = 0
+        k = k % n
         j = n-k-1
         while i < j:
             nums[i],nums[j] = nums[j],nums[i]
             i = i + 1
             j = j - 1
-
         i = n-k
-        j = len(nums)-1
+        j = n - 1
         while i < j:
             nums[i],nums[j] = nums[j],nums[i]
             i = i + 1
             j = j - 1
-        nums.reverse()
+        i = 0
+        j = n - 1
+        while i < j:
+            nums[i],nums[j] = nums[j],nums[i]
+            i = i + 1
+            j = j - 1
         return nums
-
-
-
-
-
 
         

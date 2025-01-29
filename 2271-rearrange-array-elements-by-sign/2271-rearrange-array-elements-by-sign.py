1f52caf0c47c443852d32e1be9431400be6ef892
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2025-01-29 20:07:36 +0530

    Time: 83 ms (16.02%), Space: 41.5 MB (89.54%) - LeetHub

---
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -0,0 +1,33 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+
+        # Brute Force Approach
+        temp_pos = []
+        temp_neg = []
+        for i in range(len(nums)):
+            if nums[i] >= 0:
+                temp_pos.append(nums[i])
+            else:
+                temp_neg.append(nums[i])
+    
+        for i in range(len(temp_pos)):
+            nums[2 * i] = temp_pos[i]
+        for i in range(len(temp_neg)):
+            nums[2 * i + 1] = temp_neg[i]
+        return nums
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
